8889b69a82ade235299161067ea1bfd7f4ded8e6
流光 <67579722@qq.com>
2021-12-25 21:56:47 +0800

    支持动态链接

--- build.sh
+++ build.sh
@@ -4,14 +4,20 @@ outputDir="$(pwd)/cmake-build-billing"
 buildType=MinSizeRel
 
 # 可选32位 ./build.sh m32
-buildOpt=
+# 可选shared ./build.sh shared
+buildM32=
+buildShared=
 if [ $# -ge 1 ]; then
-  if [ "$1" = "m32" ]; then
-    buildOpt=-DBILLING_BUILD_M32=ON
-  fi
+  for arg in $*; do
+    if [ "$arg" = "m32" ]; then
+      buildM32=-DBILLING_BUILD_M32=ON
+    elif [ "$arg" = "shared" ]; then
+      buildShared=-DBILLING_BUILD_SHARED=ON
+    fi
+  done
 fi
 
 mkdir -p "${outputDir}"
 cd "${outputDir}" || exit
-cmake -DCMAKE_BUILD_TYPE=${buildType} ${buildOpt} ..
+cmake -DCMAKE_BUILD_TYPE=${buildType} ${buildM32} ${buildShared} ..
 cmake --build . --target billing

840324b36106e7eabdcedbb70af5a51f1c4ea64e
流光 <67579722@qq.com>
2021-12-26 18:02:02 +0800

    fix command

--- server/cmd/run_up.cpp
+++ server/cmd/run_up.cpp
@@ -11,15 +11,9 @@
 namespace cmd {
     int runUpCommand(int argc, char *argv[], bool disableDaemon) {
         using std::string;
-
-        char *exePath = canonicalize_file_name(argv[0]);
-        string exeDir = dirname(exePath);
-        free(exePath);
-        //获取配置文件的绝对路径
-        string configFilePath = exeDir;
-        configFilePath += "/config.yaml";
         //获取日志文件参数
         string logPath = "billing.log";
+        //是否使用守护进程在后台运行
         bool runDaemon = false;
         for (int i = 0; i < argc; i++) {
             if ((strcmp(argv[i], "--log-path") == 0) && (i + 1 < argc)) {
@@ -29,6 +23,20 @@ namespace cmd {
                 runDaemon = true;
             }
         }
+        //daemon - run in the background
+        if (runDaemon) {
+            if (daemon(1, 1) != 0) {
+                std::cerr << "run daemon failed: " << strerror(errno) << std::endl;
+                return EXIT_FAILURE;
+            }
+        }
+        //可执行文件绝对路径
+        char *exePath = canonicalize_file_name(argv[0]);
+        string exeDir = dirname(exePath);
+        free(exePath);
+        //获取配置文件的绝对路径
+        string configFilePath = exeDir;
+        configFilePath += "/config.yaml";
         string logFilePath;
         //相对路径
         if (logPath[0] != '/') {
@@ -37,13 +45,6 @@ namespace cmd {
         }
         logFilePath += logPath;
         services::Server server;
-        if (runDaemon) {
-            //daemon - run in the background
-            if (daemon(1, 1) != 0) {
-                std::cerr << "run daemon failed: " << strerror(errno) << std::endl;
-                return EXIT_FAILURE;
-            }
-        }
         server.initResource(configFilePath.c_str(), logFilePath.c_str());
         return server.run();
     }
